36b7166368b1fa523db8bd95b9d5729cfcb8cd73
weldish <sc19ws2@leeds.ac.uk>
2023-02-27 18:40:24 +0000

    modified the pre-existing item  class

--- src/lib/item.cpp
+++ src/lib/item.cpp
@@ -1,5 +1,35 @@
 //
-// Created by weldeab solomon on 22/02/2023.
+// Created by weldeab solomon on 22/01/2023.
 //
 
 #include "item.h"
+
+
+using namespace onedpacking;
+
+
+Item::Item(int item_id, int item_size):
+        m_item_id(item_id),
+        m_item_size(item_size)
+
+{}
+
+
+int Item::getItemId() const
+{
+
+    return m_item_id;
+}
+
+// returns the size of the actual item
+int Item::getItemSize() const
+{
+    return m_item_size;
+}
+
+
+bool onedpacking::item_Comparator_Decreasing(Item* item1, Item* item2)
+{
+    return (item1->getItemSize() > item2->getItemSize());
+}
+
