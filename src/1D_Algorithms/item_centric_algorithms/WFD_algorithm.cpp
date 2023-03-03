fb35abdecd77b2f9e6d8a1dd48cbcd8900a58420
weldish <sc19ws2@leeds.ac.uk>
2023-03-03 10:39:08 +0000

    implemented the WFD algorithm and unit tested it

--- src/1D_Algorithms/item_centric_algorithms/WFD_algorithm.cpp
+++ src/1D_Algorithms/item_centric_algorithms/WFD_algorithm.cpp
@@ -2,4 +2,17 @@
 // Created by weldeab solomon on 03/03/2023.
 //
 
-#include "WFD_algorithm.h"
+#include "../../include/WFD_algorithm.h"
+
+WFD_algorithm::WFD_algorithm(std::string algo_name, const Instance &instance):
+        ItemCentricAlgo(algo_name, instance)
+
+{
+    m_is_WFD_type = true;
+
+}
+
+void WFD_algorithm::sortBins()
+{
+    stable_sort(m_bins_activated.begin(), m_bins_activated.end(), bin_Comparator_Rem_Cap_Decreasing);
+}
