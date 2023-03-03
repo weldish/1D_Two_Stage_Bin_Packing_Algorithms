bb2c8bb09eeb24baa46fd85898e0f6c210d1b66e
weldish <sc19ws2@leeds.ac.uk>
2023-03-03 10:27:32 +0000

    implemented the BFD algorithm and unit tested

--- src/1D_Algorithms/item_centric_algorithms/BFD_algorithm.cpp
+++ src/1D_Algorithms/item_centric_algorithms/BFD_algorithm.cpp
@@ -2,4 +2,19 @@
 // Created by weldeab solomon on 03/03/2023.
 //
 
-#include "BFD_algorithm.h"
+#include "../../include/BFD_algorithm.h"
+
+
+
+BFD_algorithm::BFD_algorithm(std::string algo_name, const Instance &instance):
+        ItemCentricAlgo(algo_name, instance)
+
+{
+    m_is_BFD_type = true;
+
+}
+
+void BFD_algorithm::sortBins()
+{
+    stable_sort(m_bins_activated.begin(), m_bins_activated.end(), bin_Comparator_Rem_Cap_Increasing);
+}
